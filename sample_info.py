a094b2bc53531a2ed3cf31118fca6a25278df767
outputzero1 <115109641+outputzero1@users.noreply.github.com>
2022-10-06 00:42:22 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,24 +1,24 @@
 # Bot information
-SESSION = 'LuciferMoringstar_Robot'
-USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:LuciferMoringstar_Robot-zyx57W2v1u123ew11'
-USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
+SESSION = 'BQBDWW0law2yzHKgIQvjK2k93PdUHhWLB2S2YybTHKXC0ie1Y8AJ89yW4yO5PFkdB9W7eQ_pNSahm9M0AgcjNP9wrGzTdWhtGT5ESsVltmllOHuDDDnYNd2l5_8zTCYlbGSSQ05wercHl1oZLjgHbg6W1nncijbe1y_Sx55g6JL2aCHEpv9Dzm-TbKVuzkjWKHorlWsmRaphd4PN4yjFwcWR1R2-E9qvfVgdd8B-CxBOd8HDzJ_o0K0xFKIGQVBd__cQve6raZmWUakPHMelligQ-5_cbUItaSVuwS5x6NNIjYPZDtsXP_JyX_X4DFAHHA4VjK_bVYJ6NpFjdpnejA6rAAAAAUQqjEYA'
+USER_SESSION = 'filterzflixbot'
+API_ID = 11004381
+API_HASH = '8e0588044fcf7672cfe1341185bfc94c'
+BOT_TOKEN = '5732081520:AAErJygtWmP8ETgwrydmBYQ49XtZOiOiYnQ'
+USERBOT_STRING_SESSION = 'filterzflixbot'
 
 # Bot settings
 CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 987654321]
-CHANNELS = [-10012345678, -100987654321, 'Mo_Tech_YT']
-AUTH_USERS = []
+ADMINS = [1260704822, '1260704822', 1260704822]
+CHANNELS = [-1001554386641, -1001554386641, '-1001554386641']
+AUTH_USERS = [-1001554386641, '-1001554386641', -1001554386641]
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[LuciferMoringstar_Robot:LuciferMoringstar_Robot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'LuciferMoringstar_Robot'
+DATABASE_URI = "mongodb+srv://filterzflixbot:filterzflixbot@filterzflixbot.nj7hwtd.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'filterzflixbot'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
